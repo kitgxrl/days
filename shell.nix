915947f12a9adecc821fd802c13eec8b36fe554a
Rain <kitgxrl@protonmail.com>
2021-09-05 16:46:51 +0200

    (cargo) initial template

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    latest.rustChannels.nightly.rust
+    latest.rustChannels.nightly.cargo
+    latest.rustChannels.nightly.rust-src
+    rustfmt
+    rust-analyzer
+
+    openssl
+    clang
+    gcc
+    pkgconfig
+  ];
+}
