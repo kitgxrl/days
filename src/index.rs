7a7d85a3ac3653854fa5b50ebae07305d53e3414
Rain <kitgxrl@protonmail.com>
2021-09-19 18:14:24 -0400

    Rework index, add tests for index parsing

--- src/index.rs
+++ src/index.rs
@@ -1,4 +1,5 @@
 use crate::store::Store;
+use std::fmt::Display;
 use std::fs::File;
 use std::io::Read;
 
@@ -7,44 +8,49 @@ pub struct Index<'a> {
     pub entries: Vec<IndexEntry>,
 }
 
+#[derive(Debug)]
 pub struct IndexEntry {
     pub name: String,
+    pub category: String,
     pub version: String,
 }
 
 impl<'a> Index<'a> {
     pub fn from(store: Store<'a>) -> Self {
-        Self {
-            store,
-            entries: Vec::new(),
-        }
+        let entries = Index::parse(&store);
+
+        Self { store, entries }
     }
 
-    fn parse(&self) -> Vec<IndexEntry> {
+    fn parse(store: &Store) -> Vec<IndexEntry> {
         let mut entries = Vec::new();
 
-        let index = self.store.directory.join("/pkgs.cnt");
+        let index = store.directory.join("pkgs.cnt");
+        println!("{:#?}", index);
         let mut file = File::open(index).expect("Failed to open index");
         let mut contents = String::new();
 
         file.read_to_string(&mut contents).unwrap();
 
         for line in contents.lines() {
-            let mut split = line.split(" ");
+            let replaced = line.replace('/', " ");
+            let mut split = replaced.split(' ');
 
-            let name = split.nth(0);
-            let version = split.nth(1);
+            let possible_name = split.next();
+            let possible_category = split.next();
+            let possible_version = split.next();
 
-            // TODO: warn line is invalid
-            // Ideally this will never happen, especially with official repos
+            let (name, category, version) =
+                match (possible_name, possible_category, possible_version) {
+                    (Some(n), Some(c), Some(v)) => (n.to_string(), c.to_string(), v.to_string()),
+                    _ => continue, // FIXME: This needs some type of warning, even if it should never happen..
+                };
 
-            if name.is_none() || version.is_none() {
-                continue;
-            } else {
-                let name = name.unwrap().to_string();
-                let version = version.unwrap().to_string();
-                entries.push(IndexEntry { name, version });
-            }
+            entries.push(IndexEntry {
+                name,
+                category,
+                version,
+            });
         }
 
         entries
--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,24 @@
+use std::path::Path;
+
+use crate::{error::StoreError, index::Index, store::Store};
+
 #[test]
 pub fn works() {
     ()
+}
+
+#[test]
+pub fn parsed_index() -> Result<(), StoreError> {
+    let ps = Store::new(Path::new("./test_pkgdb/"));
+
+    let store = match ps {
+        Ok(s) => s,
+        Err(e) => return Err(e),
+    };
+
+    let index = Index::from(store);
+
+    // println!("{:#?}", index.entries);
+
+    Ok(())
 }
