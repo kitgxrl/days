1a12714859e4a0826c283f01532785ec84fd6857
Rain <kitgxrl@protonmail.com>
2021-09-19 18:17:02 -0400

    Place holder for `Package::from(IndexEntry)`

--- src/package.rs
+++ src/package.rs
@@ -1,3 +1,5 @@
+use crate::index::IndexEntry;
+
 pub struct Package {
     name: String,
     desc: String,
@@ -6,5 +8,11 @@ pub struct Package {
     maintainers: Vec<String>,
     deps: Vec<Package>,
     // FIXME: Flags could become it's own type later
-    flags: Vec<String> 
+    flags: Vec<String>,
+}
+
+impl Package {
+    pub fn from(entry: &IndexEntry) -> Self {
+        unimplemented!()
+    }
 }
