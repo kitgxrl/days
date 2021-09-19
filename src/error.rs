b5a369abc6f506fead597a7689774b142059c232
Rain <kitgxrl@protonmail.com>
2021-09-19 18:16:02 -0400

    Store search, Reformatting, Update StoreError

--- src/error.rs
+++ src/error.rs
@@ -4,15 +4,21 @@ use std::fmt;
 pub enum StoreError {
     NotFound(String),
     InvalidDirectory,
-    Other(String)
+    Other(String),
 }
 
 impl fmt::Display for StoreError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            StoreError::NotFound(pkg) => f.write_str(&format!("The package `{}` was unable to be found.", pkg)),
-            StoreError::InvalidDirectory => f.write_str("Store directory does not exist or is not a directory"),
-            StoreError::Other(msg) => f.write_str(msg)
+            StoreError::NotFound(pkg) => {
+                f.write_str(&format!("The package `{}` was unable to be found.", pkg))
+            }
+            StoreError::InvalidDirectory => {
+                f.write_str("Store directory does not exist or is not a directory")
+            }
+            StoreError::Other(msg) => f.write_str(msg),
         }
     }
 }
+
+impl std::error::Error for StoreError {}
--- src/store.rs
+++ src/store.rs
@@ -1,4 +1,5 @@
 use crate::error::StoreError;
+use crate::index::{Index, IndexEntry};
 use crate::package::Package;
 use std::path::Path;
 
@@ -7,7 +8,7 @@ pub struct Store<'a> {
 }
 
 impl<'a> Store<'a> {
-    pub fn new(self, directory: &'a Path) -> Result<Self, StoreError> {
+    pub fn new(directory: &'a Path) -> Result<Self, StoreError> {
         if let Ok(existence) = directory.try_exists() {
             if !existence || !directory.is_dir() {
                 return Err(StoreError::InvalidDirectory);
@@ -15,20 +16,48 @@ impl<'a> Store<'a> {
         } else {
             return Err(StoreError::Other(String::from(
                 "Unable to check if store directory exists",
-            )))
+            )));
         }
 
-        self.check_validity();
+        Store::check_validity(directory);
 
         Ok(Self { directory })
     }
 
     // TODO: implement checks to verify store validity
-    fn check_validity(self) -> bool {
-        true  
+    fn check_validity(path: &'a Path) -> bool {
+        true
     }
 
-    pub fn search(name: String, version: Option<String>) -> Option<Package> {
-        unimplemented!()
+    pub fn search(
+        self,
+        name: String,
+        category: Option<String>,
+        version: Option<String>,
+    ) -> Option<Package> {
+        let index = Index::from(self);
+
+        let filtered = index
+            .entries
+            .iter()
+            .filter(|pkg| {
+                let to_search = pkg.name == name;
+
+                if let Some(cat) = &category {
+                    let to_search = to_search && &pkg.category == cat;
+                }
+                if let Some(ver) = &version {
+                    let to_search = to_search && &pkg.version == ver;
+                }
+
+                to_search
+            })
+            .collect::<Vec<&IndexEntry>>();
+
+        match filtered.len() {
+            1 => Some(Package::from(filtered[0])),
+            2.. => unimplemented!(), // TODO: Handle ambigious package names
+            _ => None,
+        }
     }
 }
